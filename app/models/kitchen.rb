4a3f33d9e6f3fbcb752d8f64b0977780f2ddde51
takose <rina.m.takase@gmail.com>
2016-03-13 05:12:41 +0000

    modify production

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -1,3 +1,9 @@
+require 'bundler/setup'
+Bundler.require
+if development?
+  ActiveRecord::Base.establish_connection('sqlite3:db/development.db')
+end
+
 class Kitchen < ActiveRecord::Base
     validates :link,
         presence: true,
